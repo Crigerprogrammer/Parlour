d1568904be7653a38e499db85370b59c667db0c1
Cristian Gerardo Hernandez Barrios <crigerprogrammer@gmail.com>
2021-10-07 19:03:13 -0600

    actualizar

--- php/conexion.php
+++ php/conexion.php
@@ -3,7 +3,6 @@ $servername = 'tcp:parlor.database.windows.net,1433';
 try  
 {  
     $conn = new PDO( "sqlsrv:server=$servername ; Database=parlour", "parlouradmin", "Pasteleria2021$$");  
-    echo 'Si funciona la conexion'
 }  catch(PDOExcepction $e) {
     echo $e;
 } 
