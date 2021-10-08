918e30b905ff16fb909de6b0c41d93be55fcb1b1
Cristian Gerardo Hernandez Barrios <crigerprogrammer@gmail.com>
2021-10-07 18:50:50 -0600

    error
    error

--- php/conexion.php
+++ php/conexion.php
@@ -2,7 +2,9 @@
 $servername = 'tcp:parlor.database.windows.net,1433';
 try  
 {  
-    $conn = new PDO( "sqlsrv:server=$servername ; Database=parlour", "parlouradmin", "Pasteleria2021$$");  
+    $conn = new PDO( "sqlsrv:server=$servername ; Database=", "parlouradmin", "Pasteleria2021$$");  
 }  catch(PDOExcepction $e) {
     echo $e;
-} echo 'Hola'
+}
+
+echo 'Hola';
