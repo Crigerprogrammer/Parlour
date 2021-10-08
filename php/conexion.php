57defcffb54b38a6768340e2f10275162d2c34ad
Cristian Gerardo Hernandez Barrios <crigerprogrammer@gmail.com>
2021-10-07 18:44:39 -0600

    ConexionBD
    Conexion a la base de datos

---
+++ php/conexion.php
@@ -0,0 +1,8 @@
+<?php 
+$servername = 'tcp:parlor.database.windows.net,1433';
+try  
+{  
+    $conn = new PDO( "sqlsrv:server=$servername ; Database=parlour", "parlouradmin", "Pasteleria2021$$");  
+}  catch(PDOExcepction $e) {
+    echo $e;
+} echo 'Hola'
