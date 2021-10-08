a876108c4aa96d2848762907b872cbb327dc53e1
Cristian Gerardo Hernandez Barrios <crigerprogrammer@gmail.com>
2021-10-07 19:01:52 -0600

    if conexion

--- php/conexion.php
+++ php/conexion.php
@@ -3,8 +3,14 @@ $servername = 'tcp:parlor.database.windows.net,1433';
 try  
 {  
     $conn = new PDO( "sqlsrv:server=$servername ; Database=parlour", "parlouradmin", "Pasteleria2021$$");  
+    echo 'Si funciona la conexion'
 }  catch(PDOExcepction $e) {
     echo $e;
-}
+} 
+
+
+if($conn == true)
+     echo 'Si se conecto';
+
 
 //echo 'Hola';
