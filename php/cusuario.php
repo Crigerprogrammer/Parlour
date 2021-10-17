8bc95a2f37f4026bc1a794a5395b0c15c419737f
Cristian Gerardo Hernandez Barrios <crigerprogrammer@gmail.com>
2021-10-16 20:55:26 -0600

    php

---
+++ php/cusuario.php
@@ -0,0 +1,18 @@
+<?php 
+require ("conexion.php");
+
+if(isset($_POST['tipo_usuario'])){
+    $tipo = $_POST['tipo_usuario'];
+    $desc = $_POST['desc_usuario'];
+
+    $sql = "INSERT INTO TIPO_USUARIO(TIPO_USUARIO, DESC_USUARIO)
+            VALUES('".$tipo."','".$desc."')";
+    $statement = $conn->prepare($sql);
+
+    if($statement->execute()){
+        $mensaje = "USUARIO Agregado Correctamente";
+        echo '<div class="alert alert-success">';
+        echo $mensaje;
+        echo '</div>';
+    }
+}
