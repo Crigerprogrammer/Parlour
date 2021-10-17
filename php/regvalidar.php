b31a1fa4dc96a85e0cc7537a0fd3e028195ea52b
Cristian Gerardo Hernandez Barrios <crigerprogrammer@gmail.com>
2021-10-16 22:57:30 -0600

    PARENTESISI

--- php/regvalidar.php
+++ php/regvalidar.php
@@ -14,7 +14,7 @@ if(isset($_POST['cui'])){
 
     $consulta = "INSERT INTO [dbo].[CLIENTE] (CUI, NOMBRES, APELLIDOS, TEL_DEFAULT, TEL_SECUNDARIO, CORREO, DIRECCION, NIT) 
         VALUES ('".$cui."', '".$nombres."', '".$apellidos."', '".$tel_default."', '".$tel_secundario."', '".$correo."',
-        '".$direccion."', '".$nit."'";
+        '".$direccion."', '".$nit."')";
     $statement = $conn->prepare($consulta);
 
 
