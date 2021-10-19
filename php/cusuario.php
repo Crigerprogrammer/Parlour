fccf1ffeb1547189b1e4b7abc8b19c8d1af66a57
Cristian Gerardo Hernandez Barrios <34076011+Crigerprogrammer@users.noreply.github.com>
2021-10-18 21:59:43 -0600

    Update cusuario.php

--- php/cusuario.php
+++ php/cusuario.php
@@ -1,18 +1,20 @@
 <?php 
 require ("conexion.php");
 
-if(isset($_POST['tipo_usuario'])){
-    $tipo = $_POST['tipo_usuario'];
-    $desc = $_POST['desc_usuario'];
+if(isset($_POST['regusuario'])){
+    $usuario = $_POST['regusuario'];
+    $contraseña = $_POST['regcontraseña'];
+    $estado = 1;
 
-    $sql = "INSERT INTO TIPO_USUARIO(TIPO_USUARIO, DESC_USUARIO)
-            VALUES('".$tipo."', CAST('".$desc."'AS VARBINARY(MAX)) )";
+    $sql = "INSERT INTO USUARIO(USUARIO, CONTRASEÑA, ESTADO)
+            VALUES('".$usuario."', CAST('".$contraseña."'AS VARBINARY(MAX)), '".$estado"')";
     $statement = $conn->prepare($sql);
 
     if($statement->execute()){
-        $mensaje = "USUARIO Agregado Correctamente";
-        echo '<div class="alert alert-success">';
+        $mensaje = "USUARIO Creado Correctamente";
+        echo '<h1 class= "bad">';
         echo $mensaje;
-        echo '</div>';
+        echo '</h1>';
+        header ("location:index.php");
     }
 }
