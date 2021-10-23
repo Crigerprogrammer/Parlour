b81ead336a093fb902a8d9db751b060c4c8f0758
Cristian Gerardo Hernandez Barrios <crigerprogrammer@gmail.com>
2021-10-23 01:50:52 -0600

    compra r

--- php/cCompra.php
+++ php/cCompra.php
@@ -12,25 +12,10 @@ if(isset($_POST['cui'])){
 
     if($statement->execute()){
         $mensaje = "COMPRA REGISTRADA";
-        echo '<div class="modal" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title">Gracias por confiar en nosotros!</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              <p>Compra realizada :) </p>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-primary">OK</button>
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">CERRAR</button>
-            </div>
-          </div>
-        </div>
-      </div>';
         
+        header ("location:index2.php");
+        echo '<h1 class= "bad">';
+        echo $mensaje;
+        echo '</h1>';
     }
 }
